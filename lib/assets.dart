2ce2f5c3123b33684e884f9da3c2355e1568a292
Arturo Gutiérrez <arturo.gutierrez@gmail.com>
2021-02-08 11:01:28 +0100

    Image and colors added

---
+++ lib/assets.dart
@@ -0,0 +1,16 @@
+class Assets {
+  static const String _path = 'assets/images';
+
+  static const String cell_bomb = '$_path/cell_bomb.png';
+  static const String cell_closed = '$_path/cell_closed.png';
+  static const String cell_flagged = '$_path/cell_flagged.png';
+  static const String cell_opened = '$_path/cell_opened.png';
+  static const String cell_opened_1 = '$_path/cell_opened_1.png';
+  static const String cell_opened_2 = '$_path/cell_opened_2.png';
+  static const String cell_opened_3 = '$_path/cell_opened_3.png';
+  static const String cell_opened_4 = '$_path/cell_opened_4.png';
+  static const String cell_opened_5 = '$_path/cell_opened_5.png';
+  static const String cell_opened_6 = '$_path/cell_opened_6.png';
+  static const String cell_opened_7 = '$_path/cell_opened_7.png';
+  static const String cell_opened_8 = '$_path/cell_opened_8.png';
+}
---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundStartColor = Color(0xFF30353B);
+const kBackgroundEndColor = Color(0xFF1A1C20);
