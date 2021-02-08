5223df8a270296251f1b119a5fb824b00f03df16
Arturo Gutiérrez <arturo.gutierrez@gmail.com>
2021-02-08 15:37:26 +0100

    Some renames and bloc dep. added

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,16 +1,29 @@
 class Assets {
   static const String _path = 'assets/images';
 
-  static const String cell_bomb = '$_path/cell_bomb.png';
-  static const String cell_closed = '$_path/cell_closed.png';
-  static const String cell_flagged = '$_path/cell_flagged.png';
-  static const String cell_opened = '$_path/cell_opened.png';
-  static const String cell_opened_1 = '$_path/cell_opened_1.png';
-  static const String cell_opened_2 = '$_path/cell_opened_2.png';
-  static const String cell_opened_3 = '$_path/cell_opened_3.png';
-  static const String cell_opened_4 = '$_path/cell_opened_4.png';
-  static const String cell_opened_5 = '$_path/cell_opened_5.png';
-  static const String cell_opened_6 = '$_path/cell_opened_6.png';
-  static const String cell_opened_7 = '$_path/cell_opened_7.png';
-  static const String cell_opened_8 = '$_path/cell_opened_8.png';
+  static const String cellBomb = '$_path/cell_bomb.png';
+  static const String cellClosed = '$_path/cell_closed.png';
+  static const String cellFlagged = '$_path/cell_flagged.png';
+  static const String cellOpened = '$_path/cell_opened.png';
+  static const String cellOpened1 = '$_path/cell_opened_1.png';
+  static const String cellOpened2 = '$_path/cell_opened_2.png';
+  static const String cellOpened3 = '$_path/cell_opened_3.png';
+  static const String cellOpened4 = '$_path/cell_opened_4.png';
+  static const String cellOpened5 = '$_path/cell_opened_5.png';
+  static const String cellOpened6 = '$_path/cell_opened_6.png';
+  static const String cellOpened7 = '$_path/cell_opened_7.png';
+  static const String cellOpened8 = '$_path/cell_opened_8.png';
+
+  static const List<String> openedCells = [
+    cellOpened,
+    cellOpened1,
+    cellOpened2,
+    cellOpened3,
+    cellOpened4,
+    cellOpened5,
+    cellOpened6,
+    cellOpened7,
+    cellOpened8,
+    cellBomb,
+  ];
 }
